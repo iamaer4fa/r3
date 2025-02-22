1722bb9a4edcd764e027cea0c1ba93bd37e6ba81
allanregistos <allan.registos@gmail.com>
2025-02-22 15:38:54 +0800

    Added embedded support for Linux

--- db/embedded/embedded.go
+++ db/embedded/embedded.go
@@ -7,26 +7,38 @@ package embedded
 import (
 	"path/filepath"
 	"r3/config"
+	"runtime"
 )
 
 var (
-	dbBin    string // pgsql binary directory
-	dbBinCtl string // pgsql service control
-	dbData   string // pgsql data directory
+	// paths
+	dbBin    string // database binary
+	dbBinCtl string // database control binary
+	dbData   string // database data directory
+	locale   string // locale setting for database messages
 
-	locale string = "en_US"
-
-	msgStarted = "server started"
-	msgStopped = "server stopped"
-	msgState0  = "no server running"
-	msgState1  = "server is running"
+	// database state messages
+	msgState0  string = "is stopped"
+	msgState1  string = "is running"
+	msgStarted string = "started"
+	msgStopped string = "stopped"
 )
 
 func GetDbBinPath() string {
 	return dbBin
 }
+
+// SetPaths sets the paths for the embedded database
 func SetPaths() {
-	dbBin = config.File.Paths.EmbeddedDbBin
-	dbBinCtl = filepath.Join(dbBin, "pg_ctl")
 	dbData = config.File.Paths.EmbeddedDbData
+
+	if runtime.GOOS == "windows" {
+		dbBin = filepath.Join("pgsql", "bin", "postgres.exe")
+		dbBinCtl = filepath.Join("pgsql", "bin", "pg_ctl.exe")
+		locale = "English"
+	} else {
+		dbBin = "postgres"
+		dbBinCtl = "pg_ctl"
+		locale = "C"
+	}
 }
--- db/embedded/embedded_linux.go
+++ db/embedded/embedded_linux.go
@@ -1,12 +1,147 @@
-//go:build linux || darwin
+//go:build linux
 
 package embedded
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"r3/config"
+	"r3/log"
+	"r3/tools"
+	"strings"
+	"time"
+)
 
 func Start() error {
-	return fmt.Errorf("embedded database is only supported on Windows")
+	// check for existing embedded database path
+	exists, err := tools.Exists(dbData)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		// get database from template
+		if err := tools.FileMove(strings.Replace(dbData, "database", "database_template", 1),
+			dbData, false); err != nil {
+
+			return err
+		}
+	}
+
+	// check embedded database state
+	state, err := status()
+	if err != nil {
+		return err
+	}
+
+	if state {
+		return fmt.Errorf("database already running, another instance is likely active")
+	}
+	_, err = execWaitFor(dbBinCtl, []string{"start", "-D", dbData,
+		fmt.Sprintf(`-o "-p %d"`, config.File.Db.Port)}, []string{msgStarted}, 10)
+
+	return err
 }
+
 func Stop() error {
-	return fmt.Errorf("embedded database is only supported on Windows")
+	state, err := status()
+	if err != nil {
+		return err
+	}
+
+	if !state {
+		log.Info("server", "embedded database already stopped")
+		return nil
+	}
+
+	_, err = execWaitFor(dbBinCtl, []string{"stop", "-D", dbData}, []string{msgStopped}, 10)
+	return err
+}
+
+func status() (bool, error) {
+	foundLine, err := execWaitFor(dbBinCtl, []string{"status", "-D", dbData},
+		[]string{msgState0, msgState1}, 5)
+
+	if err != nil {
+		return false, err
+	}
+	// returns true if DB server is running
+	return strings.Contains(foundLine, msgState1), nil
+}
+
+// executes call and waits for specified lines to return
+// will return automatically after timeout
+func execWaitFor(call string, args []string, waitFor []string, waitTime int) (string, error) {
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(waitTime)*time.Second)
+	cmd := exec.CommandContext(ctx, call, args...)
+	tools.CmdAddSysProgAttrs(cmd)
+	cmd.Env = append(os.Environ(), fmt.Sprintf("LC_MESSAGES=%s", locale))
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+
+	type chanReturnType struct {
+		err  error
+		line string
+	}
+	chanReturn := make(chan chanReturnType)
+
+	// react to call timeout
+	go func() {
+		for {
+			<-ctx.Done()
+			chanReturn <- chanReturnType{err: errors.New("timeout reached")}
+			return
+		}
+	}()
+
+	// react to new lines from standard output
+	go func() {
+		if err := cmd.Start(); err != nil {
+			chanReturn <- chanReturnType{err: err}
+			return
+		}
+
+		buf := bufio.NewReader(stdout)
+		bufLines := []string{}
+		for {
+			line, _, err := buf.ReadLine()
+			if err != nil {
+				if err != io.EOF {
+					// log error if not end-of-file
+					log.Error("server", "failed to read from std out", err)
+				}
+				break
+			}
+			bufLines = append(bufLines, string(line))
+
+			// success if expected lines turned up
+			for _, waitLine := range waitFor {
+				if strings.Contains(string(line), waitLine) {
+					chanReturn <- chanReturnType{
+						err:  nil,
+						line: waitLine,
+					}
+					return
+				}
+			}
+		}
+
+		if len(bufLines) == 0 {
+			// nothing turned up
+			chanReturn <- chanReturnType{err: errors.New("output is empty")}
+		} else {
+			// expected lines did not turn up
+			chanReturn <- chanReturnType{err: fmt.Errorf("unexpected output, got: %s", strings.Join(bufLines, ","))}
+		}
+	}()
+
+	res := <-chanReturn
+	return res.line, res.err
 }
